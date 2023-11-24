7de3cdf3838e87a88d2b43c5e56b6a95f11682c6
matbach <mateusz.bachorczyk@gmail.com>
2023-11-24 23:16:10 +0100

    using postgresql engine

--- mymap/mymap/settings.py
+++ mymap/mymap/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'mymap.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.contrib.gis.db.backends.postgis',
-    #     'HOST': 'db',
-    #     'NAME': os.environ.get('POSTGRES_DB'),
-    #     'USER': os.environ.get('POSTGRES_USER'),
-    #     'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-    #     'PORT': '5432',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'HOST': 'db',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'PORT': '5432',
+    }
 }
 
 
