1a9057ae098cf733660403eb065568a213cb7d30
matbach <mateusz.bachorczyk@gmail.com>
2023-11-24 23:41:29 +0100

    displaying all markers added by admin

--- mymap/markers/static/map.js
+++ mymap/markers/static/map.js
@@ -1,5 +1,7 @@
 const copy = "© <a href='https://osm.org/copyright'>OpenStreetMap</a> contributors";
 const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const osm = L.tileLayer(url, { attribution: copy });
-const map = L.map("map", { layers: [osm] });
-map.fitWorld();
+const map = L.map("map", { layers: [osm], minZoom: 5 });
+const markers = JSON.parse(document.getElementById("markers-data").textContent);
+const features = L.geoJSON(markers).bindPopup(layer => layer.feature.properties.name);
+map.addLayer(features).fitBounds(features.getBounds());
