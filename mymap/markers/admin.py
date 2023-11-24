314b3b344d3038bcd09bb56216a069a23c893340
matbach <mateusz.bachorczyk@gmail.com>
2023-11-24 23:41:03 +0100

    new markers model, markers for admin

--- mymap/markers/admin.py
+++ mymap/markers/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import Marker
 
-# Register your models here.
+
+@admin.register(Marker)
+class MarkerAdmin(admin.OSMGeoAdmin):
+    list_display = ['name', 'location']
