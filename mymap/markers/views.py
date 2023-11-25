23cdd0f0906e05502e5af46d386d6a30839d48ad
matbach <mateusz.bachorczyk@gmail.com>
2023-11-25 12:03:18 +0100

    database port changed, get_context_data of markers view removed because of serving data through api

--- mymap/markers/views.py
+++ mymap/markers/views.py
@@ -8,8 +8,8 @@ from .models import Marker
 class MarkersMapView(TemplateView):
     template_name = 'map.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        markers = Marker.objects.all()
-        context['markers'] = json.loads(serialize('geojson', markers))
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     markers = Marker.objects.all()
+    #     context['markers'] = json.loads(serialize('geojson', markers))
+    #     return context
