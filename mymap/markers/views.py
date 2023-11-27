fb1bb7fcc0cfba1715deed2e564f70fb15761ed3
matbach <mateusz.bachorczyk@gmail.com>
2023-11-27 11:06:35 +0100

    views refactor

--- mymap/markers/views.py
+++ mymap/markers/views.py
@@ -1,12 +1,5 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
-import json
-from django.core.serializers import serialize
 from .models import Marker
-from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser
-from rest_framework.decorators import api_view
-from rest_framework import status
 from rest_framework import generics
 from .serializers import MarkerSerializer
 
